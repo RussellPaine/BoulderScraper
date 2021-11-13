aaf5a34e38e78eb7ae46c998867f23c1a5315fe7
RussellPaine <russell.d.paine@gmail.com>
2021-11-13 19:48:02 +0000

    Write to csv

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import requests
 import re 
-import json 
+import json
+import csv
+from datetime import datetime
 
 def main():
     response = requests.get("https://portal.rockgympro.com/portal/public/620b59568a6c93407373bda88564f747/occupancy")
     text = re.findall("{\n.*'AAA' :([^;]*),(.*)}", response.text)[0][0]
     thisjson = json.loads(text.replace("'", "\""))
 
-    print(thisjson["count"])
+    with open('count.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        data = [datetime.now().strftime("%d/%m/%Y"), datetime.now().strftime("%H:%M:%S"), thisjson["count"], thisjson["capacity"]]
+        writer.writerow(data)
+
     return
 
 if __name__ == "__main__":
